1103a5f90a4ebd515307569e71cfa02418f8d28a
airajaneeeee <ajbassig@gmail.com>
2024-02-20 10:40:41 -0800

    Comp 1800 demo 07 Firebase Quick Start and Authentication

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -1,14 +1,14 @@
 //----------------------------------------
 //  Your web app's Firebase configuration
 //----------------------------------------
-var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyAcaXNgz5YMvEs5BxPZiSsGUdkyf4kLxDg",
+    authDomain: "comp1800-demo07-33973.firebaseapp.com",
+    projectId: "comp1800-demo07-33973",
+    storageBucket: "comp1800-demo07-33973.appspot.com",
+    messagingSenderId: "865135977026",
+    appId: "1:865135977026:web:0264bb366fb9b4cec3c3db"
+  };
 
 //--------------------------------------------
 // initialize the Firebase app
